c9fd1303338585ab5cd5e9aae0a1161edd2bec58
Jim Fisher <jameshfisher@gmail.com>
2023-12-07 00:29:06 +0000

    constants

--- lib/AudioSystem.ts
+++ lib/AudioSystem.ts
@@ -1,36 +1,53 @@
 import { Glottis, Tract, sampleRate, setSampleRate } from "./globals";
 
+// blockLength Can be set to 512 for more responsiveness but
+// potential crackling if CPU can't fill the buffer fast enough (latency)
+const blockLength = 2048;
+
+function doScriptProcessor(event: AudioProcessingEvent) {
+  const inputArray1 = event.inputBuffer.getChannelData(0);
+  const inputArray2 = event.inputBuffer.getChannelData(1);
+  const outArray = event.outputBuffer.getChannelData(0);
+  for (let j = 0, N = outArray.length; j < N; j++) {
+    const lambda1 = j / N;
+    const lambda2 = (j + 0.5) / N;
+    const glottalOutput = Glottis.runStep(lambda1, inputArray1[j]);
+
+    let vocalOutput = 0;
+    //Tract runs at twice the sample rate
+    Tract.runStep(glottalOutput, inputArray2[j], lambda1);
+    vocalOutput += Tract.lipOutput + Tract.noseOutput;
+    Tract.runStep(glottalOutput, inputArray2[j], lambda2);
+    vocalOutput += Tract.lipOutput + Tract.noseOutput;
+    outArray[j] = vocalOutput * 0.125;
+  }
+  Glottis.finishBlock();
+  Tract.finishBlock();
+}
+
 export class AudioSystemClass {
   started: boolean;
-  blockLength: number;
   blockTime: number;
-  soundOn: boolean;
   audioContext: AudioContext;
   scriptProcessor?: ScriptProcessorNode;
 
-  // blockLength Can be set to 512 for more responsiveness but
-  // potential crackling if CPU can't fill the buffer fast enough (latency)
-
   constructor() {
-    this.blockLength = 2048;
-    this.blockTime = 1;
     this.started = false;
-    this.soundOn = false;
 
     this.audioContext = new window.AudioContext();
     setSampleRate(this.audioContext.sampleRate);
 
-    this.blockTime = this.blockLength / sampleRate;
+    this.blockTime = blockLength / sampleRate;
   }
 
   startSound() {
     //scriptProcessor may need a dummy input channel on iOS
     this.scriptProcessor = this.audioContext.createScriptProcessor(
-      this.blockLength,
+      blockLength,
       2,
       1
     );
-    this.scriptProcessor.onaudioprocess = this.doScriptProcessor;
+    this.scriptProcessor.onaudioprocess = doScriptProcessor;
     this.scriptProcessor.connect(this.audioContext.destination);
 
     const whiteNoise = this.createWhiteNoiseNode(2 * sampleRate); // 2 seconds of noise
@@ -69,27 +86,6 @@ export class AudioSystemClass {
     source.loop = true;
 
     return source;
-  }
-
-  doScriptProcessor(event: AudioProcessingEvent) {
-    const inputArray1 = event.inputBuffer.getChannelData(0);
-    const inputArray2 = event.inputBuffer.getChannelData(1);
-    const outArray = event.outputBuffer.getChannelData(0);
-    for (let j = 0, N = outArray.length; j < N; j++) {
-      const lambda1 = j / N;
-      const lambda2 = (j + 0.5) / N;
-      const glottalOutput = Glottis.runStep(lambda1, inputArray1[j]);
-
-      let vocalOutput = 0;
-      //Tract runs at twice the sample rate
-      Tract.runStep(glottalOutput, inputArray2[j], lambda1);
-      vocalOutput += Tract.lipOutput + Tract.noseOutput;
-      Tract.runStep(glottalOutput, inputArray2[j], lambda2);
-      vocalOutput += Tract.lipOutput + Tract.noseOutput;
-      outArray[j] = vocalOutput * 0.125;
-    }
-    Glottis.finishBlock();
-    Tract.finishBlock();
   }
 
   mute() {
